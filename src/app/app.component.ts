4ee3246f6bc66947b95b892d3faf42f08240d6ec
Enea Jahollari <jahollarienea14@gmail.com>
2024-01-28 10:31:20 +0100

    fix cls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar.component';
 import { Footer } from "./components/footer.component";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     template: `
     <app-navbar />
-    <router-outlet />
+    <div class="min-h-screen">
+      <router-outlet />
+    </div>
     <app-footer />
   `,
     imports: [RouterOutlet, Navbar, Footer]
 })
-export class AppComponent {}
+export class AppComponent {
+  isBrowser = isPlatformBrowser(inject(PLATFORM_ID));
+}
